2c40122030dbc75bed4f6afec55b4b3c39d8e3e5
AlexMeow <oo587868@hotmail.com>
2024-07-02 16:36:32 +0800

    更改footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
-                        <p>Copyright © 2077 <a href="#">Chris Meow</a> . All rights reserved.
+                        <p>Copyright © 2077 <a href="#">Chris Meow</a>. All rights reserved.
 
                             <br />Design: <a href="https://templatemo.com" target="_blank" title="free CSS templates">TemplateMo</a>  Distributed By <a href="https://themewagon.com" target="_blank" >ThemeWagon</a>
                             <br />建議使用電腦瀏覽本網站。</p>
